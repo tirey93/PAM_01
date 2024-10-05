7be75b1ba54b3b469353dc6d60b26c3761df8ccd
tirey93 <tirey93.pay@gmail.com>
2024-10-05 09:50:18 +0200

    init commit 5

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //123456
+        //1234567
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
