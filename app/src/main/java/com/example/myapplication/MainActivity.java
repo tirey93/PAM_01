2f3d38688118ab88696be4971ce484381eeee7d7
tirey93 <tirey93.pay@gmail.com>
2024-10-05 11:45:15 +0200

    dodano przycisk z onclick

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
+    private TextView vText;
+    private int numbers = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //12345678
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        mButton =  findViewById(R.id.button);//przypisanie przycisku z view
+        vText =  findViewById(R.id.text);
+
+        mButton.setOnClickListener(v -> {
+            vText.setText("Clicked " + numbers + " times");
+            numbers++;
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
