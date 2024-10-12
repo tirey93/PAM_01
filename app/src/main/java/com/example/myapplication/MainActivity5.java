6fe344c7ee2f707f0e4818e1f56aaaf35911be68
tirey93 <tirey93.pay@gmail.com>
2024-10-12 11:50:25 +0200

    fragmenty

--- app/src/main/java/com/example/myapplication/MainActivity5.java
+++ app/src/main/java/com/example/myapplication/MainActivity5.java
@@ -7,6 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.myapplication.fragments.FragmentBottom;
+import com.example.myapplication.fragments.FragmentTop;
 
 public class MainActivity5 extends AppCompatActivity {
 
@@ -15,10 +20,13 @@ public class MainActivity5 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main5);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        FragmentTop fragmentTop = new FragmentTop();
+        FragmentBottom fragmentBottom = new FragmentBottom();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.add(R.id.layoutTop, fragmentTop);
+        ft.add(R.id.layoutBottom, fragmentBottom);
+        ft.commit();
     }
 }
