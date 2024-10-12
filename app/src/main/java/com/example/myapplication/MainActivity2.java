9640ed5a01c17ab23da1f2ddb51e17ca5cbafe11
tirey93 <tirey93.pay@gmail.com>
2024-10-12 08:48:31 +0200

    dodano activity2 i finish

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button bButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+
+        bButton = findViewById(R.id.act2Button);
+        bButton.setOnClickListener(v -> {
+            finish();
+        });
+
+    }
+}
